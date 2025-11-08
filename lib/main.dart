f5fa09766372e98201e69f0b7e2a603b555b7baf
Sujal Bendre <sujalbendre2526@gmail.com>
2025-11-08 11:45:54 +0530

    Second commit

---
+++ lib/main.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+import 'Doctor Appoinment App/View/main_page.dart';
+// import 'package:flutter_ui_design/Doctor%20Appoinment%20App/View/main_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // Root widget of the app
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: MainPage(),
+    );
+  }
+}
+
+// for hive database
+// void main() async {
+//   WidgetsFlutterBinding.ensureInitialized();
+//   await Hive.initFlutter();
+//   await Hive.openBox("Box");
+//   runApp(const MyApp());
+// }
+
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return const MaterialApp(
+//       debugShowCheckedModeBanner: false,
+//       home: HiveDatabaseFlutter(),
+//     );
+//   }
+// }
